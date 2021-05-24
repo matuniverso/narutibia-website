3eb243831b0b38b90890c1e3944db0ab65c5b2f4
MatheusTheGeek <miliandroxd@gmail.com>
2021-05-24 16:32:47 -0300

    wip

--- tests/Feature/GuildTest.php
+++ tests/Feature/GuildTest.php
@@ -1,50 +1,18 @@
 <?php
 
-namespace Tests\Feature;
-
 use App\Models\Account;
 use App\Models\Player;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Tests\TestCase;
 
-class GuildTest extends TestCase
-{
-    use WithFaker, RefreshDatabase;
+use function Pest\Faker\faker;
 
-    public function testPlayerCanCreateGuild()
-    {
-        $user = Account::factory()
-            ->has(Player::factory()->count(1))
-            ->create();
+test('player can create a guild', function () {
+    $user = Account::factory()->create();
+    $player = Player::factory()->for($user)->create();
 
-        $payload = [
-            'name' => $this->faker->realTextBetween(5, 16),
-            'ownerid' => $user->players()->first()->id
-        ];
+    $response = being($user)->post(route('guilds.store'), [
+        'name' => faker()->realTextBetween(5, 16),
+        'ownerid' => $player->id
+    ]);
 
-        $response = $this->actingAs($user, 'web')
-            ->followingRedirects()
-            ->post(route('guilds.store'), $payload);
-
-        $response->assertSee('Guild criada em');
-
-        return $response;
-    }
-
-    public function testPlayerCannotHaveMoreThanOneGuild()
-    {
-        // $user = Account::factory()
-        //     ->has(Player::factory()->count(1))
-        //     ->create();
-
-        // $payload = [
-        //     'name' => $this->faker->realTextBetween(5, 16),
-        //     'ownerid' => $user->players()->first()->id
-        // ];
-
-        $response = $this->testPlayerCanCreateGuild();
-
-        dd($response);
-    }
-}
+    $response->assertSee('Guild criada em');
+});
